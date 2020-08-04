bda9306939305de9a6b08d047cbef8773960ba8b
jefranc <jefranc_10@hotmail.com>
2020-08-04 16:31:53 -0500

    nuevo login

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('evaluacion1','TemplateController@index');
 
 //Route::get('evaluacion1', 'TemplateController@index')->name('index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
