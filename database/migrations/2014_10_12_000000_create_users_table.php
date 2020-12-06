62697144f4b6dc5627474b2224255568ca8ff80e
jefranc <jefranc_10@hotmail.com>
2020-12-05 19:14:02 -0500

    aa

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('cedula', 10)->unique();
             $table->string('password');
+            $table->string('imagen')->nullable();
             $table->string('rol')->nullable();
             $table->rememberToken();
             $table->timestamps();
