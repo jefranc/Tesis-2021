a295f0f9bc28568d672b61cc19f0babd4c20d407
jefranc <jefranc_10@hotmail.com>
2021-01-31 18:56:57 -0500

    aaa

--- resources/views/resultados.blade.php
+++ resources/views/resultados.blade.php
@@ -31,43 +31,38 @@
                 </ul>
                 <div class="clearfix"></div>
             </div>
-            <div class="">
+            <div class="x_panel">
                 @foreach($res2 as $res2)
                 {{ $res2->tipo }} {{ $res2->pregunta_id }} - {{ $res2->resultado }}</br>
                 {{ $res2->ciclo }} </br>
                 {{ $res2->categoria}} </br>
                 @endforeach
             </div>
-            <div class="x_content">
-                <div class="col-md-9 col-sm-9 ">
-                    <div id="graph_bar" style="width:100%; height:280px;"></div>
 
-                    <div class="" role="tabpanel" data-example-id="togglable-tabs">
-                        <ul id="autoevaluacion" class="nav nav-tabs bar_tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#pedagogicos1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">Pedagogicos</a>
-                            </li>
-                            <li role="presentation" class=""><a href="#didacticas1" role="tab" id="profile-tab" data-toggle="tab" aria-expanded="false">Didacticas</a>
-                            </li>
-                            <li role="presentation" class=""><a href="#tics1" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false">TICS</a>
-                            </li>
-                        </ul>
-                        <div id="myTabContent" class="tab-content">
-                            <div role="tabpanel" class="tab-pane active " id="pedagogicos1" aria-labelledby="home-tab">
-                                @foreach($peda2 as $peda2)
-                                        {{ $peda2->tipo }} - {{ $peda2->resultado }},,,
-                                @endforeach
-                            </div>
-                            <div role="tabpanel" class="tab-pane fade" id="didacticas1" aria-labelledby="profile-tab">
-                                @foreach($dida2 as $dida2)
-                                        {{ $dida2->tipo }} - {{ $dida2->resultado }},,,
-                                @endforeach
-                            </div>
-                            <div role="tabpanel" class="tab-pane fade" id="tics1" aria-labelledby="profile-tab">
-                                @foreach($tic2 as $tic2)
-                                        {{ $tic2->tipo }} - {{ $tic2->resultado }},,,
-                                @endforeach
-                            </div>
-                        </div>
+            <div class="" role="tabpanel" data-example-id="togglable-tabs">
+                <ul id="autoevaluacion" class="nav nav-tabs bar_tabs" role="tablist">
+                    <li role="presentation" class="active"><a href="#pedagogicos1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">Pedagogicos</a>
+                    </li>
+                    <li role="presentation" class=""><a href="#didacticas1" role="tab" id="profile-tab" data-toggle="tab" aria-expanded="false">Didacticas</a>
+                    </li>
+                    <li role="presentation" class=""><a href="#tics1" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false">TICS</a>
+                    </li>
+                </ul>
+                <div id="myTabContent" class="tab-content">
+                    <div role="tabpanel" class="tab-pane active " id="pedagogicos1" aria-labelledby="home-tab">
+                        @foreach($peda2 as $peda2)
+                        {{ $peda2->tipo }} - {{ $peda2->resultado }},,,
+                        @endforeach
+                    </div>
+                    <div role="tabpanel" class="tab-pane fade" id="didacticas1" aria-labelledby="profile-tab">
+                        @foreach($dida2 as $dida2)
+                        {{ $dida2->tipo }} - {{ $dida2->resultado }},,,
+                        @endforeach
+                    </div>
+                    <div role="tabpanel" class="tab-pane fade" id="tics1" aria-labelledby="profile-tab">
+                        @foreach($tic2 as $tic2)
+                        {{ $tic2->tipo }} - {{ $tic2->resultado }},,,
+                        @endforeach
                     </div>
                 </div>
             </div>
@@ -95,37 +90,30 @@
                 {{ $res->categoria}} </br>
                 @endforeach
             </div>
-            <div class="x_content">
-                <div class="col-md-9 col-sm-9 ">
-
-                    <div id="graph_bar" style="width:100%; height:280px;"></div>
-
-                    <div class="" role="tabpanel" data-example-id="togglable-tabs">
-                        <ul id="autoevaluacion" class="nav nav-tabs bar_tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#pedagogicos2" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">Pedagogicos</a>
-                            </li>
-                            <li role="presentation" class=""><a href="#didacticas2" role="tab" id="profile-tab" data-toggle="tab" aria-expanded="false">Didacticas</a>
-                            </li>
-                            <li role="presentation" class=""><a href="#tics2" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false">TICS</a>
-                            </li>
-                        </ul>
-                        <div id="myTabContent" class="tab-content">
-                            <div role="tabpanel" class="tab-pane active " id="pedagogicos2" aria-labelledby="home-tab">
-                                @foreach($peda as $peda)
-                                        {{ $peda->tipo }} - {{ $peda->resultado }},,,
-                                @endforeach
-                            </div>
-                            <div role="tabpanel" class="tab-pane fade" id="didacticas2" aria-labelledby="profile-tab">
-                                @foreach($dida as $dida)
-                                        {{ $dida->tipo }} - {{ $dida->resultado }},,,
-                                @endforeach
-                            </div>
-                            <div role="tabpanel" class="tab-pane fade" id="tics2" aria-labelledby="profile-tab">
-                                @foreach($tic as $tic)
-                                        {{ $tic->tipo }} - {{ $tic->resultado }},,,
-                                @endforeach
-                            </div>
-                        </div>
+            <div class="" role="tabpanel" data-example-id="togglable-tabs">
+                <ul id="autoevaluacion" class="nav nav-tabs bar_tabs" role="tablist">
+                    <li role="presentation" class="active"><a href="#pedagogicos2" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">Pedagogicos</a>
+                    </li>
+                    <li role="presentation" class=""><a href="#didacticas2" role="tab" id="profile-tab" data-toggle="tab" aria-expanded="false">Didacticas</a>
+                    </li>
+                    <li role="presentation" class=""><a href="#tics2" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false">TICS</a>
+                    </li>
+                </ul>
+                <div id="myTabContent" class="tab-content">
+                    <div role="tabpanel" class="tab-pane active " id="pedagogicos2" aria-labelledby="home-tab">
+                        @foreach($peda as $peda)
+                        {{ $peda->tipo }} - {{ $peda->resultado }},,,
+                        @endforeach
+                    </div>
+                    <div role="tabpanel" class="tab-pane fade" id="didacticas2" aria-labelledby="profile-tab">
+                        @foreach($dida as $dida)
+                        {{ $dida->tipo }} - {{ $dida->resultado }},,,
+                        @endforeach
+                    </div>
+                    <div role="tabpanel" class="tab-pane fade" id="tics2" aria-labelledby="profile-tab">
+                        @foreach($tic as $tic)
+                        {{ $tic->tipo }} - {{ $tic->resultado }},,,
+                        @endforeach
                     </div>
                 </div>
             </div>
